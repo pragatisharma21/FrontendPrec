e68f5076a4c79aa5016dc8822364f2d2e81952f3
pragatisharma21 <pragatis212@gmail.com>
2024-11-21 19:48:08 +0530

    first attempt for each

---
+++ function1.js
@@ -0,0 +1,6 @@
+var sweets = ["kova" , "gulabjamun" , "laddu" , "mysorepark", "badhsaw"]
+sweets.forEach(function(element, index, array){
+    console.log(element)
+    console.log(index)
+    console.log(array)
+});
