bb43305f3d64be664fa38b2ac9aced84e91cfe5d
pragatisharma21 <pragatis212@gmail.com>
2024-12-12 12:38:52 +0530

    just prec for web

---
+++ app.js
@@ -0,0 +1,20 @@
+//alert("something is wrong pragati");
+////console.log("something is wrong pragati");
+////console.error("something is wrong pragati");
+//console.warn("something is wrong pragati");
+/* let firstname = prompt("enter your first name")
+ let lastname = prompt("enter your last name")
+ let msg =  " welcome " + firstname + " " + lastname + "!"
+ alert(msg)*/
+  let name = "pragatisharma"
+  console.log(name.toUpperCase())
+
+
+ for(let i = 4; i<=40; i= i+4){
+    console.log(i)
+
+ }
+ //backwards
+ for(let i = 5; i<=50; i=i+5){
+    console.log(i)
+ }
