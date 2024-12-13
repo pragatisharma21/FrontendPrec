0652b5d6beb72cbb315f5f1e9b726c35325afa8c
pragatisharma21 <pragatis212@gmail.com>
2024-12-13 22:37:36 +0530

    new prec

--- guessing.js
+++ guessing.js
@@ -14,3 +14,31 @@ while(true){
         guess = prompt("your guess was wrong please try again later")
     }
 }
+function sum(){
+    console.log("pragati")
+}
+sum()
+function multiplication(n){
+    for(i=n; i<=10*n; i=i+n){
+        console.log(i)
+    }
+}
+multiplication(5)
+function isadult(age){
+    if(age>18){
+        return "adult"
+    }else{
+        return "not adult"
+    }
+
+}
+console.log(isadult(15))
+str = ["hi", "hello", "bye", "!"]
+function concat(str){
+    let res = ""
+    for(let i = 0; i<str.length; i++){
+        res += str[i]
+    }
+    return res;
+}
+concat(str)
