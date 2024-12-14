e363d5305df6a986da8540d47a0a07ab092441b2
pragatisharma21 <pragatis212@gmail.com>
2024-12-14 20:47:00 +0530

    practice of dom manipulation

---
+++ prec.js
@@ -0,0 +1,17 @@
+//let p = document.createElement('p')
+//p.innerText = "Hey i'm red!"
+//document.querySelector('body').append(p)
+//p.classList.add("red")
+//////let heading = document.createElement('h3')
+//heading.innerText = "I'm a blue H3! "
+//document.querySelector('body').append(heading)
+//heading.classList.add("blue")
+//let div = document.createElement('div')
+//let h1 = document.createElement('h1')
+//let p = document.createElement('p')
+//h1.innerText = "I'm in a div"
+//p.innerText = "MEE TOO!"
+//div.append(h1)
+//div.append(p)
+//div.classList.add("div")
+//document.querySelector('body').append(div)
