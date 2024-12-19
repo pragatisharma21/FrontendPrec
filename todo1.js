07af31c389510444ae79f6ee855abab3cf9d4b1f
pragatisharma21 <pragatis212@gmail.com>
2024-12-19 22:03:58 +0530

    new

--- todo1.js
+++ todo1.js
@@ -1,17 +1,41 @@
-let btn = document.querySelector("button")
-let ul = document.querySelector("ul")
-let input = document.querySelector("input")
- btn.addEventListener("click" , function (){
-    let item = document.createElement("li")
-    item.innerText = input.value
-    let delbtn = document.createElement("button")
-    delbtn.innerText ="delete"
-    delbtn.classList.add("delete")
-    item.appendChild(delbtn)
-    ul.appendChild(item)
-    input.value = ""
+// let btn = document.querySelector("button")
+// let ul = document.querySelector("ul")
+// let input = document.querySelector("input")
+//  btn.addEventListener("click" , function (){
+//     let item = document.createElement("li")
+//     item.innerText = input.value
+//     let delbtn = document.createElement("button")
+//     delbtn.innerText ="delete"
+//     delbtn.classList.add("delete")
+//     item.appendChild(delbtn)
+//     ul.appendChild(item)
+//     input.value = ""
 
- })
- let delbtns = document.querySelectorAll(".delete")
+//  })
+//  let delbtns = document.querySelectorAll(".delete")
 
 
+let h1 = document.querySelector("h1")
+
+// setTimeout(()=>{
+//    h1.style.color = "red"
+// }, 1000)
+// setTimeout(()=>{
+//    h1.style.color = "green"
+// }, 2000)
+// setTimeout(()=>{
+//    h1.style.color = "pink"
+// }, 3000)
+function colorChange(color, delay, nextColorChange){
+  setTimeout(()=>{
+   h1.style.color = color
+   if(nextColorChange)nextColorChange()
+  }, delay)
+}
+colorChange("red",1000,()=>{
+   colorChange("blue",2000,()=>{
+      colorChange("red",2000,()=>{
+         colorChange("blue",2000)
+      })
+   })
+})
